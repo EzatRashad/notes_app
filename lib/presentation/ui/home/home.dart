e8f858a6e352b97422e5cd18cfc637e30dd98165
Ezat Rashad <123140512+EzatRashad@users.noreply.github.com>
2023-08-12 14:37:38 +0300

    add  floatingActionButton and showModalBottomSheet

--- lib/presentation/ui/home/home.dart
+++ lib/presentation/ui/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_hive/core/themes/colors.dart';
 import 'package:notes_hive/core/utiles/utiles.dart';
+import 'package:notes_hive/presentation/ui/home/widgets/bottomSheet_body.dart';
 import 'package:notes_hive/presentation/ui/home/widgets/custom_appbar.dart';
 import 'package:notes_hive/presentation/ui/home/widgets/listview_item.dart';
 import 'package:notes_hive/presentation/ui/home/widgets/note_item.dart';
@@ -12,6 +13,22 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backG,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet<void>(
+            backgroundColor: Colors.transparent,
+            barrierColor: Colors.transparent,
+            enableDrag: true,
+            showDragHandle: true,
+            context: context,
+            builder: (BuildContext context) {
+              return const BottomSheetBody();
+            },
+          );
+        },
+        backgroundColor: AppColors.greenDone,
+        child: const Icon(Icons.add),
+      ),
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(8.0),
---
+++ lib/presentation/ui/home/widgets/bottomSheet_body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetBody extends StatelessWidget {
+  const BottomSheetBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24), topRight: Radius.circular(24)),
+        color: Colors.amber,
+      ),
+      height: 500,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text('Modal BottomSheet'),
+            ElevatedButton(
+              child: const Text('Close BottomSheet'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
