b7a592ef7dad7ce2bd373ac1f5ce94e3ec1eae86
Ezat Rashad <123140512+EzatRashad@users.noreply.github.com>
2023-08-12 15:09:35 +0300

    add custom text  filed to BottomSheet

--- lib/presentation/ui/home/widgets/bottomSheet_body.dart
+++ lib/presentation/ui/home/widgets/bottomSheet_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes_hive/core/utiles/utiles.dart';
+
+import '../../../widgets/text_form_filed.dart';
 
 class BottomSheetBody extends StatelessWidget {
   const BottomSheetBody({super.key});
@@ -6,24 +9,33 @@ class BottomSheetBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(15),
+      width: double.infinity,
+      height: 500,
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(24), topRight: Radius.circular(24)),
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24),
+        ),
         color: Colors.amber,
       ),
-      height: 500,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const Text('Modal BottomSheet'),
-            ElevatedButton(
-              child: const Text('Close BottomSheet'),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const CustomTextFiled(
+            hintT: 'Note Title',
+          ),
+          30.ph,
+          const CustomTextFiled(
+            hintT: 'Note content',
+            maxLines: 5,
+          ),
+          const Spacer(),
+          ElevatedButton(
+            child: const Text('Close BottomSheet'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/presentation/widgets/text_form_filed.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_hive/core/themes/colors.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  const CustomTextFiled({super.key, required this.hintT,  this.maxLines=1});
+  final String hintT;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: AppColors.darkGrey,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+          border: border(),
+          enabledBorder: border(),
+          focusedBorder: border(AppColors.darkGrey),
+          hintText: hintT,
+          hintStyle: TextStyle(color: AppColors.darkGrey)),
+    );
+  }
+}
+
+OutlineInputBorder border([color]) {
+  return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(color: color ?? AppColors.white));
+}
